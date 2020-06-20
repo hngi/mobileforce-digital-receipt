c7e75654db15da9197899a5ecc0b9be4e4e4704d
TobiAdeniji94 <adenijitobi94@gmail.com>
2020-06-20 01:24:23 +0100

    adjusted the distance between widgets

--- digital_receipt/lib/widgets/main_drawer.dart
+++ digital_receipt/lib/widgets/main_drawer.dart
@@ -7,127 +7,157 @@ class MainDrawer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Drawer(
       child: Container(
-        padding: EdgeInsets.only(top: 60.0),
+        padding: EdgeInsets.only(top: 80.0, left: 5.0),
         child: ListView(
           children: <Widget>[
             Container(
-              padding: EdgeInsets.only(left: 12),
+              padding: EdgeInsets.only(left: 15.0),
               child: Text('Reecpy',
                 style: TextStyle(
                     fontFamily: 'LibreBaskerville',
-                    fontSize: 20,
+                    fontSize: 20.0,
                     fontWeight: FontWeight.bold,
                     color: Colors.white,
                     letterSpacing: 0.6
                 ),
               ),
             ),
-            SizedBox(height: 25),
+            SizedBox(height: 25.0),
             ListTile(
-              leading: Icon(
-                Icons.account_circle,
-                color: Colors.white,
-                size: 20.0,
-              ),
-              title: Text(
-                'Profile',
-                style: TextStyle(
-                    fontFamily: 'Montserrat',
-                    fontSize: 16,
-                    fontWeight: FontWeight.w300,
-                    color: Colors.white,
-                    letterSpacing: 0.5
-                ),
+              title: Row(
+                children: <Widget>[
+                  Icon(
+                      Icons.account_circle,
+                      color: Colors.white,
+                      size: 20.0
+                  ),
+                  SizedBox(width: 15.0),
+                  Text(
+                    'Profile',
+                    style: TextStyle(
+                        fontFamily: 'Montserrat',
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.w300,
+                        color: Colors.white,
+                        letterSpacing: 0.5
+                    ),
+                  ),
+                ],
               ),
               onTap: (){},
             ),
             ListTile(
-              leading: Icon(
-                Icons.receipt,
-                color: Colors.white,
-                size: 20.0,
-              ),
-              title: Text(
-                'Receipts',
-                style: TextStyle(
-                    fontFamily: 'Montserrat',
-                    fontSize: 16,
-                    fontWeight: FontWeight.w300,
-                    color: Colors.white,
-                    letterSpacing: 0.5
-                ),
-              ),
-              onTap: (){},
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.history,
-                color: Colors.white,
-                size: 20.0,
-              ),
-              title: Text(
-                'History',
-                style: TextStyle(
-                    fontFamily: 'Montserrat',
-                    fontSize: 16,
-                    fontWeight: FontWeight.w300,
-                    color: Colors.white,
-                    letterSpacing: 0.5
-                ),
+              title: Row(
+                children: <Widget>[
+                  Icon(
+                      Icons.receipt,
+                      color: Colors.white,
+                      size: 20.0
+                  ),
+                  SizedBox(width: 15.0),
+                  Text(
+                    'Receipts',
+                    style: TextStyle(
+                        fontFamily: 'Montserrat',
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.w300,
+                        color: Colors.white,
+                        letterSpacing: 0.5
+                    ),
+                  ),
+                ],
               ),
               onTap: (){},
             ),
             ListTile(
-              leading: Icon(
-                Icons.show_chart,
-                color: Colors.white,
-                size: 20.0,
-              ),
-              title: Text(
-                'Analytics',
-                style: TextStyle(
-                    fontFamily: 'Montserrat',
-                    fontSize: 16,
-                    fontWeight: FontWeight.w300,
-                    color: Colors.white,
-                    letterSpacing: 0.5
-                ),
-              ),
-              onTap: (){},
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.description,
-                color: Colors.white,
-                size: 20.0,
-              ),
-              title: Text(
-                'Drafts',
-                style: TextStyle(
-                    fontFamily: 'Montserrat',
-                    fontSize: 16,
-                    fontWeight: FontWeight.w300,
-                    color: Colors.white,
-                    letterSpacing: 0.5
-                ),
+              title: Row(
+                children: <Widget>[
+                  Icon(
+                      Icons.history,
+                      color: Colors.white,
+                      size: 20.0
+                  ),
+                  SizedBox(width: 15.0),
+                  Text(
+                    'History',
+                    style: TextStyle(
+                        fontFamily: 'Montserrat',
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.w300,
+                        color: Colors.white,
+                        letterSpacing: 0.5
+                    ),
+                  ),
+                ],
               ),
               onTap: (){},
             ),
             ListTile(
-              leading: Icon(
-                Icons.settings,
-                color: Colors.white,
-                size: 20.0,
+              title: Row(
+                children: <Widget>[
+                  Icon(
+                      Icons.show_chart,
+                      color: Colors.white,
+                      size: 20.0
+                  ),
+                  SizedBox(width: 15.0),
+                  Text(
+                    'Analytics',
+                    style: TextStyle(
+                        fontFamily: 'Montserrat',
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.w300,
+                        color: Colors.white,
+                        letterSpacing: 0.5
+                    ),
+                  ),
+                ],
               ),
-              title: Text(
-                'Settings',
-                style: TextStyle(
-                    fontFamily: 'Montserrat',
-                    fontSize: 16,
-                    fontWeight: FontWeight.w300,
-                    color: Colors.white,
-                    letterSpacing: 0.5
-                ),
+              onTap: (){},
+            ),
+            ListTile(
+              title: Row(
+                children: <Widget>[
+                  Icon(
+                      Icons.description,
+                      color: Colors.white,
+                      size: 20.0
+                  ),
+                  SizedBox(width: 15.0),
+                  Text(
+                    'Drafts',
+                    style: TextStyle(
+                        fontFamily: 'Montserrat',
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.w300,
+                        color: Colors.white,
+                        letterSpacing: 0.5
+                    ),
+                  ),
+                ],
+              ),
+              onTap: (){},
+            ),
+            ListTile(
+              title: Row(
+                children: <Widget>[
+                  Icon(
+                      Icons.settings,
+                      color: Colors.white,
+                      size: 20.0
+                  ),
+                  SizedBox(width: 15.0),
+                  Text(
+                    'Settings',
+                    style: TextStyle(
+                        fontFamily: 'Montserrat',
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.w300,
+                        color: Colors.white,
+                        letterSpacing: 0.5
+                    ),
+                  ),
+                ],
               ),
               onTap: (){},
             ),
