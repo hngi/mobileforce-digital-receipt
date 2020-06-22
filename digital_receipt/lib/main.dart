1876fa8dd54e050b46736a87e9f421e5bff76264
ringnyu <ringnyum@gmail.com>
2020-06-22 12:27:13 +0100

    Preference screen

--- digital_receipt/lib/main.dart
+++ digital_receipt/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:digital_receipt/screens/home_page.dart';
+//import 'package:digital_receipt/screens/preference_page.dart';
 
 void main() => runApp (MaterialApp(
   title: 'Reepcy',
@@ -8,4 +9,6 @@ void main() => runApp (MaterialApp(
   ),
   debugShowCheckedModeBanner: false,
   home: HomePage(),
+  //home: PreferencePage(),
 ));
+
