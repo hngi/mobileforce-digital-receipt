214de8c4ee48510ba9abe3f007e6f6c3ad9e122d
Festus <festusowumi@gmail.com>
2020-06-24 10:10:51 +0100

    fixed functionality to recieve and show a notification from the backend

--- digital_receipt/lib/services/api_service.dart
+++ digital_receipt/lib/services/api_service.dart
@@ -2,11 +2,17 @@ import 'dart:convert';
 import 'dart:io';
 import 'package:dio/adapter.dart';
 import 'package:dio/dio.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_device_type/flutter_device_type.dart';
 import 'device_info_service.dart';
+import 'shared_preference_service.dart';
 
 class ApiService {
   static DeviceInfoService deviceInfoService = DeviceInfoService();
   static String _urlEndpoint = "https://frozen-island-67494.herokuapp.com/v1";
+  static FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+  static SharedPreferenceService _sharedPreferenceService =
+      SharedPreferenceService();
 
   final Dio _dio = Dio(
     BaseOptions(
@@ -25,15 +31,30 @@ class ApiService {
       return client;
     };
     String deviceId = await deviceInfoService.getId();
+    String fcmToken = await _firebaseMessaging.getToken();
+    String deviceType;
+    String auth_token;
+    String userId;
+
+    //Check deviceType
+    if (Device.get().isAndroid) {
+      deviceType = 'android';
+    } else if (Device.get().isIos) {
+      deviceType = 'ios';
+    }
+
     try {
       print(email_address);
       print(password);
+      print(fcmToken);
+      print(deviceType);
       Response response = await _dio.post(
         "/user/login",
         data: {
           "password": '$password',
           "email_address": '$email_address',
-          "deviceId": deviceId
+          "deviceType": deviceType,
+          "registration_id": fcmToken,
         },
         options: Options(
           followRedirects: false,
@@ -46,6 +67,16 @@ class ApiService {
 
       if (response.data["status"] == 200) {
         print(response.data["status"]);
+
+        userId = response.data["data"]["_id"];
+        auth_token = response.data["data"]["auth_token"];
+
+        //Save details to Shared Preference
+        _sharedPreferenceService.addStringToSF("USER_ID", userId);
+        _sharedPreferenceService.addStringToSF("AUTH_TOKEN", auth_token);
+        //
+        print(auth_token);
+        print(userId);
         return "true";
       } else {
         print(response.data);
--- digital_receipt/lib/widgets/main_drawer.dart
+++ digital_receipt/lib/widgets/main_drawer.dart
@@ -31,56 +31,58 @@ class MainDrawer extends StatelessWidget {
                       ),
                     ),
                   ),
-                ),
-                SizedBox(height: 20.0),
-                ListTile(
-                  title: Row(
-                    children: <Widget>[
-                      Icon(Icons.account_circle,
-                          color: Colors.white, size: 20.0),
-                      SizedBox(width: 15.0),
-                      Text(
-                        'Account',
-                        style: TextStyle(
-                            fontFamily: 'Montserrat',
-                            fontSize: 16.0,
-                            fontWeight: FontWeight.w300,
+                  SizedBox(height: 20.0),
+                  ListTile(
+                    title: Row(
+                      children: <Widget>[
+                        Icon(
+                            Icons.account_circle,
                             color: Colors.white,
-                            letterSpacing: 0.5),
-                      ),
-                    ],
+                            size: 20.0
+                        ),
+                        SizedBox(width: 15.0),
+                        Text(
+                          'Account',
+                          style: TextStyle(
+                              fontFamily: 'Montserrat',
+                              fontSize: 16.0,
+                              fontWeight: FontWeight.w300,
+                              color: Colors.white,
+                              letterSpacing: 0.5
+                          ),
+                        ),
+                      ],
+                    ),
+                    onTap: (){},
                   ),
-                  onTap: () {},
-                ),
-                ListTile(
-                  title: Row(
-                    children: <Widget>[
-                      Icon(Icons.receipt, color: Colors.white, size: 20.0),
-                      SizedBox(width: 15.0),
-                      Text(
-                        'Receipts',
-                        style: TextStyle(
-                            fontFamily: 'Montserrat',
-                            fontSize: 16.0,
-                            fontWeight: FontWeight.w300,
+                  ListTile(
+                    title: Row(
+                      children: <Widget>[
+                        Icon(
+                            Icons.receipt,
                             color: Colors.white,
-                            letterSpacing: 0.5),
-                      ),
-                    ],
+                            size: 20.0
+                        ),
+                        SizedBox(width: 15.0),
+                        Text(
+                          'Receipts',
+                          style: TextStyle(
+                              fontFamily: 'Montserrat',
+                              fontSize: 16.0,
+                              fontWeight: FontWeight.w300,
+                              color: Colors.white,
+                              letterSpacing: 0.5
+                          ),
+                        ),
+                      ],
+                    ),
+                    onTap: (){},
                   ),
-                  onTap: () {},
-                ),
-                ListTile(
-                  title: Row(
-                    children: <Widget>[
-                      Icon(Icons.history, color: Colors.white, size: 20.0),
-                      SizedBox(width: 15.0),
-                      Text(
-                        'History',
-                        style: TextStyle(
-                            fontFamily: 'Montserrat',
-                            fontSize: 16.0,
-                            fontWeight: FontWeight.w300,
+                  ListTile(
+                    title: Row(
+                      children: <Widget>[
+                        Icon(
+                            Icons.history,
                             color: Colors.white,
                             size: 20.0
                         ),
@@ -100,57 +102,58 @@ class MainDrawer extends StatelessWidget {
                     onTap: (){
                        Navigator.push(context, MaterialPageRoute(builder: (context) => ReceiptHistory()));
                     },
-
                   ),
-                  onTap: () {},
-                ),
-                ListTile(
-                  title: Row(
-                    children: <Widget>[
-                      Icon(Icons.show_chart, color: Colors.white, size: 20.0),
-                      SizedBox(width: 15.0),
-                      Text(
-                        'Analytics',
-                        style: TextStyle(
-                            fontFamily: 'Montserrat',
-                            fontSize: 16.0,
-                            fontWeight: FontWeight.w300,
+                  ListTile(
+                    title: Row(
+                      children: <Widget>[
+                        Icon(
+                            Icons.show_chart,
                             color: Colors.white,
-                            letterSpacing: 0.5),
-                      ),
-                    ],
+                            size: 20.0
+                        ),
+                        SizedBox(width: 15.0),
+                        Text(
+                          'Analytics',
+                          style: TextStyle(
+                              fontFamily: 'Montserrat',
+                              fontSize: 16.0,
+                              fontWeight: FontWeight.w300,
+                              color: Colors.white,
+                              letterSpacing: 0.5
+                          ),
+                        ),
+                      ],
+                    ),
+                    onTap: (){},
                   ),
-                  onTap: () {},
-                ),
-                ListTile(
-                  title: Row(
-                    children: <Widget>[
-                      Icon(Icons.description, color: Colors.white, size: 20.0),
-                      SizedBox(width: 15.0),
-                      Text(
-                        'Drafts',
-                        style: TextStyle(
-                            fontFamily: 'Montserrat',
-                            fontSize: 16.0,
-                            fontWeight: FontWeight.w300,
+                  ListTile(
+                    title: Row(
+                      children: <Widget>[
+                        Icon(
+                            Icons.description,
                             color: Colors.white,
-                            letterSpacing: 0.5),
-                      ),
-                    ],
+                            size: 20.0
+                        ),
+                        SizedBox(width: 15.0),
+                        Text(
+                          'Drafts',
+                          style: TextStyle(
+                              fontFamily: 'Montserrat',
+                              fontSize: 16.0,
+                              fontWeight: FontWeight.w300,
+                              color: Colors.white,
+                              letterSpacing: 0.5
+                          ),
+                        ),
+                      ],
+                    ),
+                    onTap: (){},
                   ),
-                  onTap: () {},
-                ),
-                ListTile(
-                  title: Row(
-                    children: <Widget>[
-                      Icon(Icons.settings, color: Colors.white, size: 20.0),
-                      SizedBox(width: 15.0),
-                      Text(
-                        'Preferences',
-                        style: TextStyle(
-                            fontFamily: 'Montserrat',
-                            fontSize: 16.0,
-                            fontWeight: FontWeight.w300,
+                  ListTile(
+                    title: Row(
+                      children: <Widget>[
+                        Icon(
+                            Icons.settings,
                             color: Colors.white,
                             size: 20.0
                         ),
@@ -169,21 +172,21 @@ class MainDrawer extends StatelessWidget {
                     ),
                     onTap: (){
                       Navigator.push(context, MaterialPageRoute(builder: (context) => PreferencePage()));
-                    },                  ),
-                  onTap: () {},
-                ),
-              ],
+                    },
+                  ),
+                ],
+              ),
             ),
           ),
           Align(
             alignment: Alignment(0.0, -1.0),
             child: Container(
-              width: 55.0,
+              width:55.0,
               height: 55.0,
               color: Color(0xFF0000),
               alignment: Alignment.center,
               child: FlatButton(
-                onPressed: () {
+                onPressed: (){
                   Navigator.pop(context);
                 },
                 child: Icon(
@@ -195,6 +198,6 @@ class MainDrawer extends StatelessWidget {
           ),
         ],
       ),
-    ));
+    );
   }
 }
