a2281f1c3e8de5a9126ceeda9f61f6d8d0e0aebf
fridaychukwuamaka <fridaychukwuamaka@gmail.com>
2020-06-27 23:30:58 +0100

    Updated account page

--- digital_receipt/lib/screens/account_page.dart
+++ digital_receipt/lib/screens/account_page.dart
@@ -31,161 +31,191 @@ class _AccountPageState extends State<AccountPage> {
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.blue[700],
-          leading: IconButton(
-            icon: Icon(
-              Icons.arrow_back,
-              color: Colors.white,
+          // backgroundColor: Colors.blue[700],
+
+          title: Text(
+            'Account',
+            style: TextStyle(
+              fontFamily: 'Montserrat',
+              fontSize: 18,
+              fontWeight: FontWeight.w600,
             ),
-            onPressed: null,
           ),
-          title: Text('Account'),
         ),
-        body: Padding(
-          padding:
-              const EdgeInsets.only(left: 15.0, bottom: 15, right: 15, top: 9),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Center(
-                child: Container(
-                  height: screenHeight * 0.155,
-                  width: screenWidth * 0.781,
-                  padding: EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                        colors: [Colors.teal[100], Colors.teal[300]],
-                        begin: Alignment.topRight,
-                        end: Alignment.bottomLeft,
-                      )),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Center(
-                        child: Column(
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.only(
+                left: 16.0, bottom: 116, right: 16, top: 30),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Center(
+                  child: Container(
+                    height: 133,
+                    width: double.infinity,
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5),
+                        color: Color(0xFF76DBC9)
+                        /*  gradient: LinearGradient(
+                          colors: [Colors.teal[100], Colors.teal[300]],
+                          begin: Alignment.topRight,
+                          end: Alignment.bottomLeft,
+                        ) */
+                        ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Column(
                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                           children: <Widget>[
                             Text(
                               'Unlock Amazing Features',
                               style: CustomText.displayn,
                             ),
+                            SizedBox(
+                              height: 5,
+                            ),
                             Text(
                               'Upgrade to premium',
                               style: CustomText.display1,
                             )
                           ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 45,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                    InkWell(
+                      child: Container(
+                        height: 50,
+                        constraints: BoxConstraints(
+                          maxWidth: 74,
                         ),
-                      )
+                        //width: 65,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                        child: image == null
+                            ? Icon(Icons.verified_user)
+                            : Image.file(
+                                image,
+                                fit: BoxFit.cover,
+                              ),
+                      ),
+                      onTap: getImage,
+                    ),
+                    Container(
+                      height: 20,
+                      width: 20,
+                      decoration: BoxDecoration(color: Color(0xFFE7ECF1)),
+                      child: InkWell(
+                          onTap: () {},
+                          child: Icon(
+                            Icons.edit,
+                            color: Colors.black,
+                            size: 12,
+                          )),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 12,
+                ),
+                Center(
+                  child: Text(
+                    '$username',
+                    style: CustomText.display1,
+                  ),
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+                Text(
+                  'Edit',
+                  style: TextStyle(
+                    color: Color(0xFF0B57A7),
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                SizedBox(
+                  height: 4,
+                ),
+                InformationData(
+                  label: 'Email Address',
+                  detail: 'myemail@mail.com',
+                ),
+                InformationData(
+                  label: 'Phone No',
+                  detail: '892-983-240',
+                ),
+                InformationData(
+                  label: 'Address',
+                  detail: '5, Amphitheatre Railway Street Degit',
+                ),
+                InformationData(
+                  label: 'Bussiness Slogan',
+                  detail: 'We are taking over',
+                ),
+                SizedBox(
+                  height: 35,
+                ),
+                RawMaterialButton(
+                  onPressed: () {},
+                  highlightColor: Colors.transparent,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text('Change Password', style: CustomText.display3),
+                      Icon(Icons.keyboard_arrow_right)
                     ],
                   ),
                 ),
-              ),
-              Stack(
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.only(top: 10),
-                    child: Align(
-                      alignment: Alignment.topCenter,
-                      child: InkWell(
-                        child: Container(
-                          height: 65,
-                          width: 65,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(50),
-   
-                          ),
-                          child: image == null
-                              ? Icon(Icons.verified_user)
-                              : Image.file(
-                                  image,
-                                  fit: BoxFit.cover,
-                                ),
-                        ),
-                        onTap: getImage,
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                      left: screenWidth*0.486,
-                      top: screenHeight*0.0732,
-                      child: Container(
-                        height: 20,
-                        width: 20,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(12.5),
-                            color: Colors.white38),
-                        child: Center(
-                          child: Icon(Icons.edit,
-                              color: Colors.teal, size: 15),
-                        ),
-                      )),
-                ],
-              ),
-              Center(
-                child: Text(
-                  '$username',
-                  style: CustomText.display1,
+                SizedBox(
+                  height: 47,
                 ),
-              ),
-              Text(
-                'Edit',
-                style: TextStyle(color: Colors.blue),
-              ),
-              InformationData(
-                label: 'Email Address',
-                detail: 'myemail@mail.com',
-              ),
-              Divider(color: Colors.black45),
-              InformationData(
-                label: 'Phone No',
-                detail: '892-983-240',
-              ),
-              Divider(color: Colors.black45),
-              InformationData(
-                label: 'Address',
-                detail: '5, Amphitheatre Railway Street Degit',
-              ),
-              Divider(color: Colors.black45),
-              InformationData(
-                label: 'Bussiness Slogan',
-                detail: 'We are taking over',
-              ),
-              Divider(color: Colors.black45),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Text('Change Password', style: CustomText.display3),
-                  Icon(Icons.keyboard_arrow_right)
-                ],
-              ),
-              InkWell(
-                child: Center(
+                FlatButton(
+                  onPressed: () {},
+                  highlightColor: Colors.transparent,
+                  splashColor: Colors.transparent,
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
                       Container(
                           padding: EdgeInsets.all(8),
-                          
                           child: Row(
                             children: <Widget>[
-                              Text(
-                                'Logout',
-                                style: TextStyle(fontSize: 17),
-                              ),
                               Icon(
-                                Icons.directions_run,
+                                Icons.exit_to_app,
                                 color: Colors.red,
                                 size: 20,
+                              ),
+                              SizedBox(
+                                width: 15,
+                              ),
+                              Text(
+                                'Logout',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.normal,
+                                ),
                               ),
                             ],
                           )),
                     ],
                   ),
                 ),
-                onTap: null,
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
@@ -200,19 +230,44 @@ class InformationData extends StatelessWidget {
   InformationData({this.label, this.detail});
   @override
   Widget build(BuildContext context) {
-    return  Row(
-        children: <Widget>[
-          Text(
+    return Column(
+      children: <Widget>[
+        SizedBox(
+          height: 20,
+        ),
+        Row(
+          children: <Widget>[
+            Text(
               '$label : ',
-              style: CustomText.displayn,
+              style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.black,
+                  fontWeight: FontWeight.w500,
+                  height: 1.43,
+                  letterSpacing: 0.03),
             ),
-          Expanded(
-            child: Text(
-              '$detail',
-              style: CustomText.display2,
-            ),
-          )
-        ],
+            Expanded(
+              child: Text(
+                '$detail',
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+                style: TextStyle(
+                  fontWeight: FontWeight.normal,
+                  color: Colors.black,
+                  fontSize: 15,
+                ),
+              ),
+            )
+          ],
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Container(
+          height: 1,
+          color: Color.fromRGBO(0, 0, 0, 0.12),
+        ),
+      ],
     );
   }
 }
