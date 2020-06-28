1520f4bfce25c370d5e7c80f2096382d9c94590e
fridaychukwuamaka <fridaychukwuamaka@gmail.com>
2020-06-28 21:19:04 +0100

    Test upgrade screen

--- digital_receipt/lib/screens/upgrade_screen.dart
+++ digital_receipt/lib/screens/upgrade_screen.dart
@@ -19,59 +19,66 @@ class UpgradeScreen extends StatelessWidget {
           ),
         ),
         body: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              Column(
-                children: <Widget>[
-                  Container(
-                    padding: EdgeInsets.only(top: 10.0),
-                    height: 120.0,
-                    width: 105.49,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage('assets/images/group52.png'),
-                        fit: BoxFit.fill,
-                      ),
-                      shape: BoxShape.rectangle,
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    SizedBox(
+                      height: 14,
                     ),
-                  ),
-                  SizedBox(
-                    child: Text(
-                      'You make me blush',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontFamily: 'Montserrat',
-                        fontSize: 22.0,
-                        fontWeight: FontWeight.w600,
-                        color: Colors.black,
-                        letterSpacing: 0.5,
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 10.0),
-                  SizedBox(
-                    child: Text(
-                      'Degeit is better with premium. Enjoy all features of degeit. Do business like a pro',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontFamily: 'Montserrat',
-                        fontSize: 14.0,
-                        color: Colors.grey,
-                        letterSpacing: 0.5,
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 20.0),
-                  Container(
-                    height: 83.0,
-                    child: Container(
-                      width: MediaQuery.of(context).size.width - 32,
+                    Container(
+                      height: 113.0,
+                      width: 111.49,
                       decoration: BoxDecoration(
-                          image: DecorationImage(image: AssetImage('assets/images/frame55.png',),
-                              fit: BoxFit.fitHeight
-                          ),
-                          borderRadius: BorderRadius.circular(5.0)
+                        image: DecorationImage(
+                          image: AssetImage('assets/images/group52.png'),
+                          fit: BoxFit.fill,
+                        ),
+                        shape: BoxShape.rectangle,
                       ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    SizedBox(
+                      child: Text(
+                        'You make me blush',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontFamily: 'Montserrat',
+                          fontSize: 22.0,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.black,
+                          letterSpacing: 0.5,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 10.0),
+                    SizedBox(
+                      child: Text(
+                        'Degeit is better with premium. Enjoy all features of degeit. Do business like a pro',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontFamily: 'Montserrat',
+                          fontSize: 14.0,
+                          color: Colors.grey,
+                          letterSpacing: 0.5,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20.0),
+                    Container(
+                      width: double.infinity,
+                      height: 83,
+                      decoration: BoxDecoration(
+                          image: DecorationImage(
+                              image: AssetImage(
+                                'assets/images/frame55.png',
+                              ),
+                              fit: BoxFit.cover),
+                          borderRadius: BorderRadius.circular(5.0)),
                       child: Column(
                         children: <Widget>[
                           SizedBox(height: 12.0),
@@ -86,7 +93,8 @@ class UpgradeScreen extends StatelessWidget {
                             ),
                           ),
                           SizedBox(height: 5.0),
-                          Text('Lifetime access',
+                          Text(
+                            'Lifetime access',
                             textAlign: TextAlign.center,
                             style: TextStyle(
                               fontFamily: 'Montserrat',
@@ -98,21 +106,17 @@ class UpgradeScreen extends StatelessWidget {
                         ],
                       ),
                     ),
-                  ),
-                  SizedBox(height: 30.0),
-                  Container(
-                    width: MediaQuery.of(context).size.width - 32,
-                    height: 89.0,
-                    padding: EdgeInsets.only(left: 0.0, right: 0.0),
-                    child: Container(
+                    SizedBox(height: 30.0),
+                    Container(
+                       padding: EdgeInsets.only(top: 10, left: 10, right: 25, bottom: 10),
                       decoration: BoxDecoration(
                           color: Color(0xFFDFF6F3),
-                          borderRadius: BorderRadius.circular(5.0)
-                      ),
+                          borderRadius: BorderRadius.circular(5.0)),
                       child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: <Widget>[
-                          SizedBox(height: 10.0),
-                          SizedBox(width: 10.0),
+                          // SizedBox(height: 10.0),
+                          //  SizedBox(width: 10.0),
                           Text(
                             'Issue receipts for half\npayments and set\nreminders for completion',
                             textAlign: TextAlign.start,
@@ -124,61 +128,58 @@ class UpgradeScreen extends StatelessWidget {
                             ),
                           ),
                           SizedBox(width: 32.0),
-                          Image(image: AssetImage('assets/icons/check.png'), height: 25, width: 25,)
+                          Image(
+                            image: AssetImage('assets/icons/check.png'),
+                            height: 25,
+                            width: 25,
+                          )
                         ],
                       ),
                     ),
-                  ),
-                  Container(
-                    width: MediaQuery.of(context).size.width - 32,
-                    height: 89.0,
-                    padding: EdgeInsets.only(left: 0.0, right: 0.0),
-                    child: Container(
+                    Container(
+                       padding: EdgeInsets.only(top: 20, left: 0, right: 25, bottom: 20),
                       decoration: BoxDecoration(
-                          color: Color(0xFFF2F8FF),
-                          borderRadius: BorderRadius.circular(5.0)
+                        color: Color(0xFFF2F8FF),
+                        borderRadius: BorderRadius.circular(5.0),
                       ),
                       child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: <Widget>[
-                          SizedBox(height: 10.0),
-                          SizedBox(width: 10.0),
-                          Container(
-                            width: 232,
-                            height: 44,
-                            child: Text(
-                              'Send receipt history to\ncustomers',
-                              textAlign: TextAlign.start,
-                              style: TextStyle(
-                                fontFamily: 'Montserrat',
-                                fontSize: 16.0,
-                                color: Colors.black,
-                                letterSpacing: 0.5,
-                              ),
+                          //SizedBox(height: 10.0),
+                          //SizedBox(width: 10.0),
+                          Text(
+                            'Send receipt history to\ncustomers',
+                            textAlign: TextAlign.start,
+                            style: TextStyle(
+                              fontFamily: 'Montserrat',
+                              fontSize: 16.0,
+                              color: Colors.black,
+                              letterSpacing: 0.5,
                             ),
                           ),
                           SizedBox(width: 18.0),
                           Container(
                             width: 24,
                             height: 24,
-                            child: Image(image: AssetImage('assets/icons/check.png'), height: 24, width: 24,),
+                            child: Image(
+                              image: AssetImage('assets/icons/check.png'),
+                              height: 24,
+                              width: 24,
+                            ),
                           ),
                         ],
                       ),
                     ),
-                  ),
-                  Container(
-                    width: MediaQuery.of(context).size.width - 32,
-                    height: 43.0,
-                    padding: EdgeInsets.only(left: 0.0, right: 0.0),
-                    child: Container(
+                    Container(
+                       padding: EdgeInsets.only(top: 10, left: 10, right: 25, bottom: 10),
                       decoration: BoxDecoration(
                           color: Color(0xFFDFF6F3),
-                          borderRadius: BorderRadius.circular(5.0)
-                      ),
+                          borderRadius: BorderRadius.circular(5.0)),
                       child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: <Widget>[
-                          SizedBox(height: 10.0),
-                          SizedBox(width: 10.0),
+                          // SizedBox(height: 10.0),
+                          //SizedBox(width: 10.0),
                           Text(
                             'Customize receipt',
                             textAlign: TextAlign.start,
@@ -193,25 +194,25 @@ class UpgradeScreen extends StatelessWidget {
                           Container(
                             width: 24,
                             height: 24,
-                            child: Image(image: AssetImage('assets/icons/check.png'), height: 24, width: 24,),
+                            child: Image(
+                              image: AssetImage('assets/icons/check.png'),
+                              height: 24,
+                              width: 24,
+                            ),
                           ),
                         ],
                       ),
                     ),
-                  ),
-                  Container(
-                    width: MediaQuery.of(context).size.width - 32,
-                    height: 89.0,
-                    padding: EdgeInsets.only(left: 0.0, right: 0.0),
-                    child: Container(
+                    Container(
+                      padding: EdgeInsets.only(top: 20, left: 0, right: 25, bottom: 20),
                       decoration: BoxDecoration(
                           color: Color(0xFFF2F8FF),
-                          borderRadius: BorderRadius.circular(5.0)
-                      ),
+                          borderRadius: BorderRadius.circular(5.0)),
                       child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: <Widget>[
-                          SizedBox(height: 10.0),
-                          SizedBox(width: 10.0),
+                          // SizedBox(height: 10.0),
+                          // SizedBox(width: 10.0),
                           Text(
                             'Reissue old reciepts',
                             textAlign: TextAlign.start,
@@ -226,25 +227,25 @@ class UpgradeScreen extends StatelessWidget {
                           Container(
                             width: 24,
                             height: 24,
-                            child: Image(image: AssetImage('assets/icons/check.png'), height: 24, width: 24,),
+                            child: Image(
+                              image: AssetImage('assets/icons/check.png'),
+                              height: 24,
+                              width: 24,
+                            ),
                           ),
                         ],
                       ),
                     ),
-                  ),
-                  Container(
-                    width: MediaQuery.of(context).size.width - 32,
-                    height: 43.0,
-                    padding: EdgeInsets.only(left: 0.0, right: 0.0),
-                    child: Container(
+                    Container(
+                      padding: EdgeInsets.only(top: 10, left: 10, right: 25, bottom: 10),
                       decoration: BoxDecoration(
                           color: Color(0xFFDFF6F3),
-                          borderRadius: BorderRadius.circular(5.0)
-                      ),
+                          borderRadius: BorderRadius.circular(5.0)),
                       child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: <Widget>[
-                          SizedBox(height: 10.0),
-                          SizedBox(width: 10.0),
+                          // SizedBox(height: 10.0),
+                          // SizedBox(width: 10.0),
                           Container(
                             width: 153,
                             height: 23,
@@ -263,46 +264,39 @@ class UpgradeScreen extends StatelessWidget {
                           Container(
                             width: 24,
                             height: 24,
-                            child: Image(image: AssetImage('assets/icons/check.png'), height: 24, width: 24,),
+                            child: Image(
+                              image: AssetImage('assets/icons/check.png'),
+                              height: 24,
+                              width: 24,
+                            ),
                           ),
                         ],
                       ),
                     ),
-                  ),
-                  SizedBox(height: 45.0),
-                  Container(
-                    color: Color(0xFFF2F8FF),
-                    height: 45.0,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Center(
-                          child: SizedBox(
-                            width: MediaQuery.of(context).size.width - 32,
-                            height: 45,
-                            child: FlatButton(
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(5)),
-                              onPressed: () {},
-                              color: Color(0xFF0B57A7),
-                              child: Text(
-                                "Upgrade",
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.w600,
-                                    letterSpacing: 0.3),
-                              ),
-                            ),
-                          ),
+                    SizedBox(height: 45.0),
+                    SizedBox(
+                      width: double.infinity,
+                      height: 45,
+                      child: FlatButton(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5)),
+                        onPressed: () {},
+                        color: Color(0xFF0B57A7),
+                        child: Text(
+                          "Upgrade",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
+                              letterSpacing: 0.3),
                         ),
-                      ],
+                      ),
                     ),
-                  ),
-                  SizedBox(height: 30.0)
-                ],
-              ),
-            ],
+                    SizedBox(height: 30.0)
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
