fe968026172b86349cdf095215f6b0b75037893b
fridaychukwuamaka <fridaychukwuamaka@gmail.com>
2020-06-29 14:21:56 +0100

    Tested setup page

--- digital_receipt/lib/widgets/main_drawer.dart
+++ digital_receipt/lib/widgets/main_drawer.dart
@@ -259,7 +259,7 @@ class MainDrawer extends StatelessWidget {
                     highlightColor: Colors.transparent,
                     onPressed: () {
                       Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => Setup()));
+                          MaterialPageRoute(builder: (context) => PreferencePage()));
                     },
                     child: Row(
                       children: <Widget>[
