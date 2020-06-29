6c4ec62c7400aef7d35d5c20c2ff59b65ca35428
fridaychukwuamaka <fridaychukwuamaka@gmail.com>
2020-06-29 08:38:10 +0100

    Tested change password screen

--- digital_receipt/lib/screens/change_password_screen.dart
+++ digital_receipt/lib/screens/change_password_screen.dart
@@ -12,13 +12,24 @@ class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Change Password'),
-        backgroundColor: Color(0xFF0B57A7),
+        title: Text(
+          'Change Password',
+          style: TextStyle(
+            fontFamily: 'Montserrat',
+            fontWeight: FontWeight.w600,
+            letterSpacing: 0.3,
+            fontSize: 16,
+            //color: Colors.white,
+          ),
+        ),
+       // backgroundColor: Color(0xFF0B57A7),
       ),
       body: SafeArea(
-        child: Container(
-          padding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 16.0),
-          child: PasswordForm(),
+        child: SingleChildScrollView(
+                  child: Container(
+            padding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 16.0),
+            child: PasswordForm(),
+          ),
         ),
       ),
     );
@@ -70,26 +81,22 @@ class PasswordForm extends StatelessWidget {
         _buildInputField(label: 'Current Password'),
         _buildInputField(label: 'New Password'),
         _buildInputField(label: 'Confirm New Password'),
-        Expanded(
-          child: Align(
-            alignment: Alignment.bottomCenter,
-            child: MaterialButton(
-              height: 45,
-              minWidth: double.infinity,
-              onPressed: () => () {},
-              color: Theme.of(context).primaryColor,
-              child: Text(
-                'Update',
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.w600,
-                    letterSpacing: 0.30),
-              ),
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(5.0))),
-            ),
+        SizedBox(height: 100),
+        MaterialButton(
+          height: 45,
+          minWidth: double.infinity,
+          onPressed: () => () {},
+          color: Theme.of(context).primaryColor,
+          child: Text(
+            'Update',
+            style: TextStyle(
+                color: Colors.white,
+                fontSize: 16.0,
+                fontWeight: FontWeight.w600,
+                letterSpacing: 0.30),
           ),
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(5.0))),
         )
       ],
     );
