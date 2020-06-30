c20fef2c261ecd2acfe42361e01bbc98ed362143
Teedari <godfredderi47@gmail.com>
2020-06-30 03:44:01 +0000

    design product information screen fix implementation

--- digital_receipt/lib/models/product.dart
+++ digital_receipt/lib/models/product.dart
@@ -1,7 +1,19 @@
 class Product{
-  final String id;
-  final String productDesc;
-  final int amount;
+   String id;
+   String productDesc;
+   int quantity;
+   int unitPrice;
+   int amount;
 
-  Product({this.id, this.productDesc, this.amount});
+  Product({this.id, this.productDesc,this.quantity, this.amount, this.unitPrice});
+
+  factory Product.fromJson(Map<String, dynamic> json){
+    return Product(
+      id: json['receiptId'] as String,
+      productDesc: json['name'] as String,
+      quantity: json['quantity'] as int, 
+      amount: json['amount'] as int);
+  }
+
+  
 }
