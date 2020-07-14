d58a21e7b0f86701bac781fa42b697dd1ba77276
Eyinjuoluwa <barbieadol@gmail.com>
2020-07-14 17:50:21 +0100

    device preview disabled

--- digital_receipt/lib/main.dart
+++ digital_receipt/lib/main.dart
@@ -52,7 +52,7 @@ Future<dynamic> myBackgroundMessageHandler(Map<String, dynamic> message) async {
 
 void main() => runApp(DevicePreview(
       builder: (_) => MyApp(),
-      enabled: !kReleaseMode,
+      enabled: false,
     ));
 
 class MyApp extends StatelessWidget {
