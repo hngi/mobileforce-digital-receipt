a887edc7aea3d3879a4dc0d33a0613e4d08a093c
Francisop <francisohis@gmail.com>
2020-07-15 16:42:15 +0100

    donnnnn

--- digital_receipt/lib/main.dart
+++ digital_receipt/lib/main.dart
@@ -5,6 +5,7 @@ import 'package:digital_receipt/screens/home_page.dart';
 import 'package:digital_receipt/screens/login_screen.dart';
 import 'package:digital_receipt/screens/onboarding.dart';
 import 'package:digital_receipt/screens/setup.dart';
+import 'package:hive/hive.dart';
 
 import 'dart:io';
 import 'utils/connected.dart';
@@ -20,6 +21,7 @@ import 'services/sql_database_client.dart';
 import 'services/shared_preference_service.dart';
 import 'services/sql_database_repository.dart';
 import 'package:intl/intl.dart';
+import 'package:path_provider/path_provider.dart';
 
 //BACKGROUND MESSAGE HANDLER
 Future<dynamic> myBackgroundMessageHandler(Map<String, dynamic> message) async {
@@ -49,11 +51,16 @@ Future<dynamic> myBackgroundMessageHandler(Map<String, dynamic> message) async {
 
   }
 }
+
 // DevicePreview(
 //       builder: (_) => MyApp(),
 //       enabled: !kReleaseMode,
 //     )
-void main() => runApp(MyApp());
+void main() async {
+  final appDocumentDir = await getApplicationDocumentsDirectory();
+  Hive.init(appDocumentDir.path);
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({
