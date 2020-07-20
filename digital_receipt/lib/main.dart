99acbf7c8a4f27e8da3a32c1373e156f9a12ea50
fridaychukwuamaka <fridaychukwuamaka@gmail.com>
2020-07-20 22:23:45 +0100

    Updated

--- digital_receipt/lib/main.dart
+++ digital_receipt/lib/main.dart
@@ -59,8 +59,10 @@ void main() async {
     Hive.init(appDocumentDir.path);
 
     // runApp(MyApp(),);
-    runApp(DevicePreview(builder: (BuildContext context) => MyApp(), enabled: !kReleaseMode,));
-
+    runApp(DevicePreview(
+      builder: (BuildContext context) => MyApp(),
+      enabled: kReleaseMode,
+    ));
   } catch (e) {
     print("error occurd in main: $e");
   }
@@ -101,7 +103,6 @@ class MyApp extends StatelessWidget {
             primaryColor: Color(0xFF0B57A7),
             scaffoldBackgroundColor: Color(0xFFF2F8FF),
             accentColor: Color(0xFF25CCB3),
-            
             textTheme: TextTheme(
               bodyText1: TextStyle(
                 fontFamily: 'Montserrat',
@@ -180,7 +181,6 @@ class _ScreenControllerState extends State<ScreenController> {
     }
 
     _fcm.configure(
-      
       onMessage: (Map<String, dynamic> message) async {
         print(message["data"]["id"]);
         print(message["notification"]["id"]);
@@ -259,7 +259,7 @@ class _ScreenControllerState extends State<ScreenController> {
         ]),
         builder: (BuildContext context, AsyncSnapshot snapshot) {
           // await _pushNotificationService.initialise();
-           print('snapshots: ${snapshot.data}');
+          print('snapshots: ${snapshot.data}');
 
           if (snapshot.connectionState == ConnectionState.waiting) {
             return Container(
