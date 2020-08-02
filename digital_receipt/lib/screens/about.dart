b2e923c1c17a9ff731e31bf006ff91f341b56de1
Francisop <francisohis@gmail.com>
2020-08-02 13:36:27 +0100

    fixed bug in product innventory

---
+++ digital_receipt/lib/screens/about.dart
@@ -0,0 +1,130 @@
+import 'package:digital_receipt/constant.dart';
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:package_info/package_info.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class About extends StatefulWidget {
+  @override
+  _AboutState createState() => _AboutState();
+}
+
+class _AboutState extends State<About> {
+  PackageInfo packageInfo;
+
+  setPackage() async {
+    setState(() {
+      packageInfo = PackageInfo();
+    });
+    var val = await PackageInfo.fromPlatform();
+    setState(() {
+      packageInfo = val;
+    });
+  }
+
+  _launchURL(String url) async {
+    //const url = url;
+    if (await canLaunch(url)) {
+      await launch(
+        url,
+      );
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  @override
+  void initState() {
+    setPackage();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'About',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 18,
+            fontWeight: FontWeight.w600,
+            fontFamily: 'Montserrat',
+            letterSpacing: 0.03,
+          ),
+        ),
+      ),
+      body: SafeArea(
+          child: Column(
+        children: <Widget>[
+          //AboutListTile(),
+          ListTile(
+            title: Text(
+              'Build version',
+              style: TextStyle(
+                fontFamily: 'Montserrat',
+              ),
+            ),
+            subtitle: Text(
+              packageInfo?.buildNumber ?? '',
+              style: TextStyle(
+                fontFamily: 'Montserrat',
+              ),
+            ),
+          ),
+          ListTile(
+            title: Text(
+              'Open-source licences',
+              style: TextStyle(
+                fontFamily: 'Montserrat',
+              ),
+            ),
+            subtitle: Text(
+              'Licence detail for open-source software',
+              style: TextStyle(
+                fontFamily: 'Montserrat',
+              ),
+            ),
+            onTap: () => showLicensePage(
+                context: context,
+                applicationIcon: Center(
+                  child: Container(
+                    height: 50,
+                    width: 150,
+                    margin: EdgeInsets.all(10),
+                    padding: EdgeInsets.symmetric(vertical: 10),
+                    child: Theme.of(context).brightness == Brightness.dark
+                        ? kLogoWithTextDark
+                        : SvgPicture.asset(
+                                  'assets/images/degeit_logo.svg'),
+                  ),
+                )),
+          ),
+          ListTile(
+              title: Text(
+                'Privacy Policy',
+                style: TextStyle(
+                  fontFamily: 'Montserrat',
+                ),
+              ),
+              onTap: () {
+                _launchURL('https://degeit.flycricket.io/privacy.html');
+              }),
+          ListTile(
+              title: Text(
+                'Terms of service',
+                style: TextStyle(
+                  fontFamily: 'Montserrat',
+                ),
+              ),
+              onTap: () {
+                 _launchURL('https://degeit.flycricket.io/terms.html');
+              }
+
+              //subtitle: Text(packageInfo?.buildNumber),
+              ),
+        ],
+      )),
+    );
+  }
+}
